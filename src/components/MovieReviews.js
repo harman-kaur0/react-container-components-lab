f950b97ed1419ed24152b883ee320ca684ee7a11
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-05-03 17:14:09 -0700

    commit tree 08df30a823a59d9de36eaae57e212871a99fc983

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from "react";
+
+const MovieReviews = (props) => {
+  let list = props.reviews.map((review) => {
+    return (
+      <li className="review">
+        <h2>{review.display_title}</h2>
+        <p>{review.summary_short}</p>
+      </li>
+    );
+  });
+
+  return <ul className="review-list">{list}</ul>;
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,53 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+import React, { Component } from "react";
+import "isomorphic-fetch";
+import MovieReviews from "./MovieReviews";
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = "dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ";
+const URL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=";
+const api = `&api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+  state = {
+    reviews: [],
+    searchTerm: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      searchTerm: e.target.value,
+    });
+  };
+
+  handleSubmit = (e, search) => {
+    e.preventDefault();
+    fetch(URL + search + api)
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({
+          reviews: data.results,
+        });
+      });
+    e.target.reset();
+  };
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={(e) => this.handleSubmit(e, this.state.searchTerm)}>
+          <label>Search movie reviews: </label>
+          <input type="text" onChange={(e) => this.handleChange(e)} />
+          <input type="submit" />
+        </form>
+        <div>
+          {this.state.reviews === null ? (
+            <h1>No results found</h1>
+          ) : (
+            <MovieReviews reviews={this.state.reviews} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
